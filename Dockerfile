be4f37b863c3b836ea0f30992f1bc2733335b732
Emilia Zysk <emzysk.dev@gmail.com>
2024-08-20 22:25:16 +0900

    test api and docker stuff

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# Dockerfile for FastAPI backend
+FROM python:3.10
+# FROM python:3.10-slim-buster
+
+# Set the working directory
+# WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    libhdf5-dev \
+    libgl1-mesa-glx \
+    && apt-get clean
+
+# Copy the requirements.txt file and install dependencies
+COPY requirements.txt requirements.txt
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+RUN pip install --no-cache-dir tensorflow && \
+    pip install --no-cache-dir git+https://github.com/tensorflow/addons.git
+
+
+# Copy the rest of the application code
+COPY consonance consonance
+COPY setup.py setup.py
+RUN pip install .
+
+# COPY Makefile Makefile
+# RUN make reset_local_files
+
+# Expose the port the app runs on
+EXPOSE 8000
+
+# Command to run the FastAPI app
+CMD ["uvicorn", "consonance.api.fast:app", "--host", "0.0.0.0", "--port", "8000"]
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+# CMD uvicorn taxifare.api.fast:app --host 0.0.0.0 --port $PORT
--- consonance/api/fast.py
+++ consonance/api/fast.py
@@ -1,6 +1,8 @@
+import os
 import pandas as pd
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, HTTPException, UploadFile, Query
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from consonance.ml_logic.registry import load_model
 from consonance.ml_logic.preprocessor import image_preprocess
 
@@ -44,6 +46,32 @@ async def predict(
     
     return {'midi': midi}
 
-# @app.get("/")
-# def root():
-#     return {'greeting': 'Hello'}
+@app.get("/test")
+async def get_dummy_midi(media_type: str = Query(..., description="The media type of the file: 'midi', 'wav', or 'mp3'")):
+    """
+    Return a dummy file based on the provided media_type.
+    """
+    media_types = {
+        'midi': 'audio/midi',
+        'wav': 'audio/wav',
+        'mp3': 'audio/mpeg'
+    }
+    
+    if media_type in media_types:
+        file_path = os.path.join(os.getcwd(), "consonance/api/hot-cross-buns.mid")
+        print("file_path=\n", file_path)
+        
+        if not os.path.exists(file_path):
+            # return {'error': f"File not found: {file_path}"}
+            raise HTTPException(status_code=404, detail=f"File not found: {file_path}")
+        
+        file_name = "hot-cross-buns.mid"
+        return FileResponse(file_path, media_type=media_types[media_type], filename=file_name)
+    
+    else:
+        # return {'error': "Invalid media type. Please use 'midi', 'wav', or 'mp3'."}
+        raise HTTPException(status_code=400, detail="Invalid media type. Please use 'midi', 'wav', or 'mp3'.")
+
+@app.get("/")
+def root():
+    return {'greeting': 'Hello'}
